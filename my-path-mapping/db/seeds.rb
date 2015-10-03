66ce295ad7da12067a5fe53356b473e64c9dea2b
Brenda Nguyen <brenguyen711@gmail.com>
2015-10-03 16:33:55 -0700

    Create address seeds

--- my-path-mapping/db/seeds.rb
+++ my-path-mapping/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+Address.create(street: "581 Hayes St", city: "San Francisco", state: "CA", zip: "94102")
+Address.create(street: "5017 Geary Blvd",  city:  "San Francisco", state: "CA", zip: "94118")
+Address.create(street: "3275 22nd St",  city: "San Francisco", state: "CA", zip: "94110")
+Address.create(street: "205 Hugo St",  city: "San Francisco", state: "CA", zip: "94122")
+Address.create(street: "345 3rd St",  city: "San Francisco", state: "CA", zip: "94107")
