1184a094f3cb9751e7d9b3ba78f63f709023c51c
atp1o2 <andrewpham8@gmail.com>
2015-10-04 12:06:19 -0700

    format pages

--- my-path-mapping/app/controllers/addresses_controller.rb
+++ my-path-mapping/app/controllers/addresses_controller.rb
@@ -28,7 +28,7 @@ class AddressesController < ApplicationController
 
     respond_to do |format|
       if @address.save
-        format.html { redirect_to @address, notice: 'Address was successfully created.' }
+        format.html { redirect_to new_address_path, notice: 'Address was successfully created.' }
         format.json { render :show, status: :created, location: @address }
       else
         format.html { render :new }
