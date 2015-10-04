392763f0e4f5b7a96b519c136db5c2c5e394af0c
Tom Lee <tlee418@g.ucla.edu>
2015-10-04 09:20:31 -0700

    mapbox multiple marker works

--- my-path-mapping/app/controllers/maps_controller.rb
+++ my-path-mapping/app/controllers/maps_controller.rb
@@ -1,8 +1,34 @@
 class MapsController < ApplicationController
 
 	def index
+    @addresses = Address.all
+    @geojson = []
+
+    @addresses.each do |address|
+      @geojson << {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [address.longitude, address.latitude] # this part is tricky
+      },
+      properties: {
+        title: "testing",
+        address: address.address,
+        :"marker-color" => "#FFFFFF",
+        :"marker-symbol" => "circle",
+        :"marker-size" => "medium",
+      }
+    }
+    end
+
+    p @geojson
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @geojson }
+    end
 	end
-	
+
 	def show
 	end
 
