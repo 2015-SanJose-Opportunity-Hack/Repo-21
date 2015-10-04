21a71def69c3466525d08c71d09d37690256ae65
Tom Lee <tlee418@g.ucla.edu>
2015-10-03 21:32:22 -0700

    multiple marker

--- my-path-mapping/app/assets/javascripts/maps.js
+++ my-path-mapping/app/assets/javascripts/maps.js
@@ -2,7 +2,5 @@
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
 
-L.mapbox.accessToken = 'pk.eyJ1IjoibXlwYXRobWFwIiwiYSI6ImNpZmJsazE1NTJtbWRyc203azFteDZjM3MifQ.4bHjuFSY_Y-xvCN4V4nZeg';
-var map = L.mapbox.map('map', 'mapbox.streets')
-    .setView([39.7,-104.50], 7);
+
 });
