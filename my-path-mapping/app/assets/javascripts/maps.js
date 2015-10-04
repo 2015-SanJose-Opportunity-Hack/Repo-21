fb08db52633781aa98ae378e227404206578185b
Brenda Nguyen <brenguyen711@gmail.com>
2015-10-04 13:29:45 -0700

    add seed

--- my-path-mapping/app/assets/javascripts/maps.js
+++ my-path-mapping/app/assets/javascripts/maps.js
@@ -1,6 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
-
+  L.mapbox.accessToken = 'pk.eyJ1IjoibXlwYXRobWFwIiwiYSI6ImNpZmJsazE1NTJtbWRyc203azFteDZjM3MifQ.4bHjuFSY_Y-xvCN4V4nZeg';
+  var map = L.mapbox.map('map', 'mapbox.streets')
+  .setView([37.7577,-122.4376], 13);
 
 });
