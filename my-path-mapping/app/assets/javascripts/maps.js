f92ed7395ffe9494c660223f6012e6eb5c98b974
Brenda Nguyen <brenguyen711@gmail.com>
2015-10-03 20:15:18 -0700

    Add working map with style and js

--- my-path-mapping/app/assets/javascripts/maps.js
+++ my-path-mapping/app/assets/javascripts/maps.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+
+L.mapbox.accessToken = 'pk.eyJ1IjoibXlwYXRobWFwIiwiYSI6ImNpZmJsazE1NTJtbWRyc203azFteDZjM3MifQ.4bHjuFSY_Y-xvCN4V4nZeg';
+var map = L.mapbox.map('map', 'mapbox.streets')
+    .setView([39.7,-104.50], 7);
+});
